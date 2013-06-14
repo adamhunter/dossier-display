f2bc19b7ac75ec03e855b96e72b1459d40ef69df
Adam Hunter <adamhunter@me.com>
2013-06-14 11:25:08 -0400

    initial commit

---
+++ lib/dossier/display.rb
@@ -0,0 +1,7 @@
+require "dossier/display/version"
+
+module Dossier
+  module Display
+    # Your code goes here...
+  end
+end
---
+++ lib/dossier/display/version.rb
@@ -0,0 +1,5 @@
+module Dossier
+  module Display
+    VERSION = "0.0.1"
+  end
+end
